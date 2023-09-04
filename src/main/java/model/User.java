a1872be0a7bde62b7c74d2238ad3634ed24a9cf0
Pablo Álvarez Calvo <pabloalvarezcalvo@gmail.com>
2023-09-04 13:23:30 +0200

    User

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,2 +1,33 @@
-package model;public class User {
+package model;
+
+import java.util.List;
+
+public class User {
+    //Contendrá nombre, una lista de los usuarios a los que sigue, lista de posts.
+    //private long id;
+    private String name;
+    private List<User> following;
+    private List<Post> publications;
+
+    public User(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getFollowing() {
+        return following;
+    }
+
+    public List<Post> getPublications() {
+        return publications;
+    }
+
+
 }
